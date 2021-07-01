bced46b7631373abedd3be1d751a2341be70ce72
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:56:38 -0400

    Update Movie.h

--- 3rd_Semester/OOP345/WS05/part2/Movie.h
+++ 3rd_Semester/OOP345/WS05/part2/Movie.h
@@ -1,12 +1,3 @@
-//========================================
-// Movie module
-// Movie.h
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/25
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
 #ifndef MOVIE_H
 #define MOVIE_H
 #include <string>
