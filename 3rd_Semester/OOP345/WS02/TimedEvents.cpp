5a232d8745227e0fc0b9b87b783ef588c2bbfaba
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:52:30 -0400

    Update TimedEvents.cpp

--- 3rd_Semester/OOP345/WS02/TimedEvents.cpp
+++ 3rd_Semester/OOP345/WS02/TimedEvents.cpp
@@ -1,13 +1,3 @@
-//========================================
-// TimedEvents module
-// TimedEvents.cpp
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/04
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <iomanip>
