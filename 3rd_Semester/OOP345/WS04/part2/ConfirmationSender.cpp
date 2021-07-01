9b51ea92276136ee0a10a10a95bfc311aeee8351
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:54:43 -0400

    Update ConfirmationSender.cpp

--- 3rd_Semester/OOP345/WS04/part2/ConfirmationSender.cpp
+++ 3rd_Semester/OOP345/WS04/part2/ConfirmationSender.cpp
@@ -1,12 +1,3 @@
-//========================================
-// ConfirmationSender module
-// ConfirmationSender.cpp
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/17
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
 #include <iostream>
 #include "ConfirmationSender.h"
 
