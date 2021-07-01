f0c996511cf6284f7d79ae31e1288e48efcc3f66
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:54:28 -0400

    Update Reservation.h

--- 3rd_Semester/OOP345/WS04/part1/Reservation.h
+++ 3rd_Semester/OOP345/WS04/part1/Reservation.h
@@ -1,13 +1,3 @@
-//========================================
-// Reservation module
-// Reservation.h
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/17
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
-
 #ifndef RESERVATION_H
 #define RESERVATION_H
 #include <iostream>
