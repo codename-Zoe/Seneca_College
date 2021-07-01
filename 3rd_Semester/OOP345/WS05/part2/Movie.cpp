ee2b66796f0c4400452dea14964a8f89ba4f975b
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:56:31 -0400

    Update Movie.cpp

--- 3rd_Semester/OOP345/WS05/part2/Movie.cpp
+++ 3rd_Semester/OOP345/WS05/part2/Movie.cpp
@@ -1,12 +1,3 @@
-//========================================
-// Movie module
-// Movie.cpp
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/25
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
 #include <iomanip>
 #include <string>
 #include "Movie.h"
