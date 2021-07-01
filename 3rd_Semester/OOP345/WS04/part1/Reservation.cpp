755688dda0ebeaf4497c357f075613360830f60d
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:54:20 -0400

    Update Reservation.cpp

--- 3rd_Semester/OOP345/WS04/part1/Reservation.cpp
+++ 3rd_Semester/OOP345/WS04/part1/Reservation.cpp
@@ -1,13 +1,3 @@
-//========================================
-// Reservation module
-// Reservation.cpp
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/17
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
-
 #include <iostream>
 #include <iomanip>
 #include <string>
