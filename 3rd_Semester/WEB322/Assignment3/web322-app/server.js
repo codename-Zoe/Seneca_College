8b686f04d408fe3e0ef7a914abdc90657607ef8b
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:58:57 -0400

    Update server.js

--- 3rd_Semester/WEB322/Assignment3/web322-app/server.js
+++ 3rd_Semester/WEB322/Assignment3/web322-app/server.js
@@ -3,9 +3,7 @@
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
  * of this assignment has been copied manually or electronically from any other source 
  * (including 3rd party web sites) or distributed to other students. 
- * 
- * Name: Nozomi Tsuchiya Student ID: 154666192 Date: 2021-06-22 
- * 
+ *
  * Online (Heroku) Link: https://tranquil-tundra-26636.herokuapp.com/
  * 
  * ********************************************************************************/
