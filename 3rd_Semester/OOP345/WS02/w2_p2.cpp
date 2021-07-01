a3fc2f4cd42f6bfcf7de0125d16770652ebb36d9
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:52:50 -0400

    Update w2_p2.cpp

--- 3rd_Semester/OOP345/WS02/w2_p2.cpp
+++ 3rd_Semester/OOP345/WS02/w2_p2.cpp
@@ -5,14 +5,6 @@
 // 2020/01/14 - Cornel
 // 2021/01/14 - Chris
 
-//========================================
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/04
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
-
 #include <iostream>
 #include <iomanip>
 #include <utility>
