7e40588d5104ca00fe81d175c555d8e244dce7d1
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:54:00 -0400

    Update SetSummable.h

--- 3rd_Semester/OOP345/WS03/part2/SetSummable.h
+++ 3rd_Semester/OOP345/WS03/part2/SetSummable.h
@@ -1,13 +1,3 @@
-//========================================
-// SetSummable module
-// SetSummable.h
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/10
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
-
 #ifndef SETSUMMABLE_H
 #define SETSUMMABLE_H
 #include <iostream>
