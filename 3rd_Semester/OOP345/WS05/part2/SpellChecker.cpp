38a2c45a87ddaff7b04d65090a8d98f4593578e4
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-07-01 19:56:45 -0400

    Update SpellChecker.cpp

--- 3rd_Semester/OOP345/WS05/part2/SpellChecker.cpp
+++ 3rd_Semester/OOP345/WS05/part2/SpellChecker.cpp
@@ -1,12 +1,3 @@
-//========================================
-// SpellChecker module
-// SpellChecker.cpp
-// Name: Nozomi Tsuchiya
-// ID: 154666192
-// Date: 2021/06/25
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments.
-//========================================
 #include <fstream>
 #include <iomanip>
 #include "SpellChecker.h"
